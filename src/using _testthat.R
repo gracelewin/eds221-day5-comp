0f3bedb7717278d4807f339f4a840cf62f9384df
gracelewin <glewin@bren.ucsb.edu>
2021-08-13 14:03:34 -0700

    wrote function and tested with testthat

--- src/using _testthat.R
+++ src/using _testthat.R
@@ -11,6 +11,16 @@ library(testthat)
 
 # apply(X = df, MARGIN = 2, FUN = mean, na.rm = TRUE)
 
+#' Range of arithmetic means
+#' 
+#' Finds the mean of all columns in a data frame, finds the maximum and minimum column mean from that data frame, and returns the range as a vector with length 2. 
+#'
+#' @param df 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 min_max_mean <- function(df) {
   
   min_mean_value <- min(apply(X = df, MARGIN = 2, FUN = mean, na.rm = TRUE))
